be2bc0535f3969f28f1746c933aa8cfb3c285187
vivekkumar854301 <vivekkumar2020@vitbhopal.ac.in>
2023-04-14 16:14:58 +0530

    Update sango.py

--- sango.py
+++ sango.py
@@ -1,7 +1,10 @@
 import pyttsx3
+import speech_recognition as sr
 import datetime
-import speechRecognition as sr
-
+import wikipedia
+import  webbrowser
+import os
+import smtplib
 
  
 engine =pyttsx3.init('sapi5')
@@ -44,8 +47,69 @@ def takeCommand():
         print("say that again please....")
         return "None"
     return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
     
     
 if __name__ == "__main__":
     wishMe()
-    takeCommand()
+    while True:
+    #if 1:
+        query=takeCommand().lower()
+        if 'wikipedia' in query:
+            speak('Searching wikipedia......')
+            query=query.replace("wikipedia","")
+            results=wikipedia.summary(query,sentences=2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+        
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+            
+        elif 'open linkedin' in query:
+            webbrowser.open("linkedin.com")
+        
+        elif 'open instagram' in query:
+            webbrowser.open("instagram.com")
+            
+        elif 'play song' in query:
+            webbrowser.open("jiosaavn.com")
+            
+        elif 'play music ' in query:
+            music_dir = 'F:\sad song'
+            songs =os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir,songs[0]))
+        
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+            
+        elif 'open vs code' in query:
+            codepath ="D:\Microsoft VS Code\Code.exe"
+            os.startfile(codepath)
+            
+        elif 'send Email' in query:
+            try:
+                speak("what should i say?")
+                contect=takeCommand()
+                to = "viveksingh144399@gmail.com"
+                sendEmail(to,contect)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("sorry my friend . i am not able to send this email")
+                
+            
+            
+              
